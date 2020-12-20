a48283be6b794350fbd90a5888a29830b6c0ff44
Jawwad110 <69030429+Jawwad110@users.noreply.github.com>
2020-12-20 18:28:04 +0500

    Sample nootebook

---
+++ my-project/script.js
@@ -0,0 +1,20 @@
+
+document.write("<br/>");
+
+function message(){
+    alert("This is the javascript to be done first time.Happy and excited");
+    alert("This is the javascript to be done first time.Happy and excited");
+}
+
+var x=5;
+var y=10;
+function conditions(){
+    if(x>y){
+        document.write("wrong conditions");
+    }else{
+        document.write("Correct answer");
+    }
+}
+function myfunction(){
+    document.getElementById("demo").innerHTML = "First Javascript Programs";
+}
